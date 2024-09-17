ca28c6215993ec63d4d85b8fcbae274063d4934d
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-09-17 16:27:59 -0300

    commit

--- script.js
+++ script.js
@@ -2,25 +2,20 @@ function adjustAsidePosition() {
     const header = document.querySelector('header');
     const aside = document.querySelector('aside');
 
-    // Pega a altura atual do header
     const headerHeight = header.offsetHeight;
 
-    // Aplica o top para o aside em qualquer tamanho de tela
     aside.style.top = `${headerHeight}px`;
 
-    // Verifica se a largura da tela é maior que 500px
     if (window.innerWidth > 500) {
-        // Para telas maiores, ajusta também a altura do aside
         aside.style.minHeight = `calc(100vh - ${headerHeight}px)`;
     } else {
-        // Para telas menores, deixa o aside ajustar automaticamente a altura
-        aside.style.minHeight = null; // Remove o ajuste de minHeight
-        aside.style.height = 'auto'; // Garante que o aside ajuste sua altura automaticamente
+        aside.style.minHeight = null;
+        aside.style.top = null;
+        aside.style.height = 'auto';
+        aside.style.fontSize = '0.6rem';
     }
 }
 
-// Chama a função quando a página carrega
 window.addEventListener('load', adjustAsidePosition);
 
-// Recalcula quando a janela é redimensionada
 window.addEventListener('resize', adjustAsidePosition);
