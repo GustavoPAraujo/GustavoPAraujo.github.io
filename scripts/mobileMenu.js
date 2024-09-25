ebc0771abb31738788f97c34f77b61e390039164
GustavoPAraujo <araujops.gustavo@gmail.com>
2024-09-25 12:25:42 -0300

    scripts

--- scripts/mobileMenu.js
+++ scripts/mobileMenu.js
@@ -1,8 +1,6 @@
-// Seleciona o ícone de menu e o menu de navegação
 const menuIcon = document.getElementById('menu-icon');
 const navMenu = document.getElementById('nav-menu');
 
-// Alterna a visibilidade do menu quando o ícone de hambúrguer é clicado
 menuIcon.addEventListener('click', () => {
     navMenu.classList.toggle('active');
 });
